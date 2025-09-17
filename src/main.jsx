c50e14eebbd896b37e6753186a754615a9b36504
Anton <Dgylian@gmail.com>
2025-09-17 21:38:02 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import HelpPage from './pages/HelpPage.jsx';
 import AccountPage from './pages/AccountPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import { AuthProvider } from './lib/auth.jsx';
+import ContactsPage from './pages/ContactsPage.jsx';
+import AboutPage from './pages/AboutPage.jsx';
 
 const router = createBrowserRouter([
   {
@@ -25,6 +27,8 @@ const router = createBrowserRouter([
       { path: 'help', element: <HelpPage /> },
       { path: 'login', element: <LoginPage /> },
       { path: 'account', element: <AccountPage /> },
+      { path: 'contacts', element: <ContactsPage /> },
+      { path: 'about', element: <AboutPage /> },
     ],
   },
 ]);
