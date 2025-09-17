b8d2f0e5021aaabd1d0290c725997d7985026e42
Anton <Dgylian@gmail.com>
2025-09-17 21:55:50 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ export default {
           red: '#E11D48',
           white: '#FFFFFF',
           dark: '#0B1324',
+          black: '#0B0B0B',
+          gold: '#D4AF37',
         },
+      },
+      fontFamily: {
+        display: ['"Playfair Display"', 'serif'],
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
       },
     },
   },
